992dfcdac493b3341208d19887d4f97ae3d2a3e9
Mayank Kumar <mayank15032006@gmail.com>
2026-07-16 16:50:40 +0530

    Time: 11 ms (35.77%), Space: 23.1 MB (5.05%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -4,19 +4,16 @@ public:
         int low=1;
         int high=piles[0];
         for (int x: piles)
-            high = max(high,x);
-        
-          while(low<high){
+            high = max(high,x);        
+        while(low<high){
             int mid= low+(high-low)/2;
             int hours=0;
             for (int y: piles)
                 hours += ceil((double)y/mid);
-            
             if (hours <=h)
                 high=mid;
             else
                 low=mid+1;
-
         }
             return low;
         
