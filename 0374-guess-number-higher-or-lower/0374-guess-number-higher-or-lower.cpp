a66e84a75b80b98ae2365fa6721305c96606bf78
Mayank Kumar <mayank15032006@gmail.com>
2026-07-16 11:43:21 +0530

    Time: 0 ms (100%), Space: 8 MB (6.14%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
@@ -12,19 +12,15 @@ public:
     int guessNumber(int n) {
         int low = 1;
         int high = n;
+        int mid,res;
 
         while (low <= high) {
-
-            int mid = low + (high - low) / 2;
-
-            int res = guess(mid);
-
+            mid = low + (high - low) / 2;
+            res = guess(mid);
             if (res == 0)
                 return mid;
-
             else if (res == -1)
                 high = mid - 1;
-
             else
                 low = mid + 1;
         }
