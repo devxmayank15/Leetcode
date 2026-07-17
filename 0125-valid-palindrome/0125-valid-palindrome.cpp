2605afb864734317f0c454b123f79d11a0104e31
Mayank Kumar <mayank15032006@gmail.com>
2026-07-17 14:47:49 +0530

    Time: 4 ms (14.53%), Space: 10.2 MB (31.68%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string str = "";
+        for (char ch : s) {
+            if (isalnum(ch)) 
+                str += tolower(ch);            
+        }
+        int i = 0;
+        int j = str.size() - 1;
+        while (i < j) {
+            if (str[i] != str[j])
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
