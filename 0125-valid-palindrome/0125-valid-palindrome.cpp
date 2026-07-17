93b31ee3067bf30b1299d1209d93978aff05d9c9
Mayank Kumar <mayank15032006@gmail.com>
2026-07-17 14:59:06 +0530

    Time: 0 ms (100%), Space: 10.4 MB (15.93%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
+
         string str = "";
+
+        // Build a cleaned string
         for (char ch : s) {
-            if (isalnum(ch)) 
-                str += tolower(ch);            
+            if (isalnum(ch)) {
+                str += tolower(ch);
+            }
         }
+
         int i = 0;
         int j = str.size() - 1;
+
         while (i < j) {
             if (str[i] != str[j])
                 return false;
+
             i++;
             j--;
         }
+
         return true;
     }
 };
