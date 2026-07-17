299f8ea6bb48f1cef2293ae32260e8263379c71e
Mayank Kumar <mayank15032006@gmail.com>
2026-07-17 14:44:45 +0530

    Time: 4 ms (34.75%), Space: 9.7 MB (76.44%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         
+        //1.use hasmap
+        //2. use an array of size 0 to 26 and insert at the index;
+        //3.sorting
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
 
@@ -9,8 +12,5 @@ public:
             return true;
         else
             return false;
-
-
-
     }
 };
