1f18d476d8eaf547695f5cbda7e1b2a56821a1cd
Mayank Kumar <mayank15032006@gmail.com>
2026-07-20 11:50:19 +0530

    Time: 0 ms (100%), Space: 8 MB (12.26%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -5,15 +5,6 @@ public:
         if (n <= 0)
             return false;
 
-        int count = 0;
-
-        while (n) {
-            if (n & 1)
-                count++;
-
-            n >>= 1;
-        }
-
-        return count == 1;
+        return (n & (n - 1)) == 0;
     }
 };
