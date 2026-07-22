4ab1ff2dd0a879c0601659ee53a0c203e0b71670
Mayank Kumar <mayank15032006@gmail.com>
2026-07-22 15:01:37 +0530

    Time: 0 ms (100%), Space: 8.6 MB (7.44%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int n=s.length();
+        stack<int> st;
+        int count=0;
+        int ans=0;
+
+        for (char ch:s){
+        if(ch=='('){
+                st.push(ch);
+                count++;
+        }
+        else if(ch==')'){
+            st.pop();
+            count --;
+        }
+        ans = max(ans, count);
+        }
+        return ans;
+
+    }
+};
