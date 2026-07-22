31d73900283d1cf329023eed07a4d363a2cddb96
Mayank Kumar <mayank15032006@gmail.com>
2026-07-22 10:24:48 +0530

    Time: 0 ms (100%), Space: 8.7 MB (43.91%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
 
     double solve(double x, long long n) {
-
-        if (n == 0)
-            return 1;
+        if (n == 0)     return 1;
 
         double half = solve(x, n / 2);
+        
+        if (n % 2 == 0)   return half * half;
 
-        if (n % 2 == 0)
-            return half * half;
-
-        return x * half * half;
+       return x * half * half;
     }
 
     double myPow(double x, int n) {
 
         long long N = n;
-
+//to handle negative powers
         if (N < 0) {
             x = 1 / x;
             N = -N;
