104df99ff092e22f25b9d49e9110eef1aac20f33
Mayank Kumar <mayank15032006@gmail.com>
2026-07-23 11:30:11 +0530

    Time: 5 ms (33.93%), Space: 126.6 MB (28.93%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -3,12 +3,11 @@ public:
     bool isPalindrome(ListNode* head) {
         ListNode* temp=head;
         string ans="";
-        while(temp)
-        {
+        while(temp){
             ans+= temp->val;
             temp=temp->next;
         }
-
+        
         if(isPalindrome(ans))
             return true;
         return false;
