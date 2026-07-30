0270a5b3a28e43b8104fe6f3374d21b3735bf17d
Mayank Kumar <mayank15032006@gmail.com>
2026-07-30 12:15:09 +0530

    Time: 1 ms (65.29%), Space: 19.2 MB (20.62%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -6,14 +6,21 @@ public:
         unordered_map<int , int> mp;
         vector<pair<int,int>> temp;
         for (int ch: nums)
+        {
             mp[ch]++;
+        }
         for (auto x: mp)
+        {
             temp.push_back({x.second, x.first});
-
-        sort(temp.rbegin(), temp.rend());
+        }
         
+        sort(temp.rbegin(), temp.rend());
+
         for(int i=0;i<k;i++)
+        {
             ans.push_back(temp[i].second);   
+        }
+
         
     return ans;
     }
