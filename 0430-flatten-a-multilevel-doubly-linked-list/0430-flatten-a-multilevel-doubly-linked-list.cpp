44b69298e736874a25ad5ddbfec8d186b09ddf04
Mayank Kumar <mayank15032006@gmail.com>
2026-08-11 19:29:40 +0530

    Time: 6 ms (31.22%), Space: 11.7 MB (45.41%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.cpp
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.cpp
@@ -1,50 +1,28 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* prev;
-    Node* next;
-    Node* child;
-};
-*/
-
 class Solution {
 public:
     Node* flatten(Node* head) {
         if (head == NULL) return NULL;
         
         stack<Node*> st;
-        // st.push(head);
         Node *curr=head;
 
         while(!curr==NULL)
         {
-
             if(curr->child){
-
                 if(curr->next)
                     st.push(curr->next);
-
-
-
                 curr->next=curr->child;
-
-                curr->child->prev = curr;
-                
+                curr->child->prev = curr;              
                 curr->child=NULL;
-
             }
             
             if (curr->next == NULL && !st.empty() ) 
             {
                 Node* nextNode = st.top();
                 st.pop();
-
                 curr->next = nextNode;
                 nextNode->prev = curr;
             }
-            cout<<curr->val<<endl;
             curr=curr->next;
             
         }
