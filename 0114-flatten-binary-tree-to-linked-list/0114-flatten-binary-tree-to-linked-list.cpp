3a319d9521395a6e128db8ca66e88257b27f11b0
Mayank Kumar <mayank15032006@gmail.com>
2026-09-06 18:56:24 +0530

    Time: 0 ms (100%), Space: 17.5 MB (78.41%) - LeetHub

---
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    TreeNode* nextright=NULL;
+    void flatten(TreeNode* root) {
+         if(root == NULL)
+            return;
+
+        flatten(root->right);
+        flatten(root->left);
+
+        root->left=NULL;
+        root->right=nextright;
+        nextright=root;
+        
+    }
+};
